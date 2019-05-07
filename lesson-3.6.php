5c61c20327c3a2b996195b605db75cbea6dc3daf
andrey-mamon <44556868+andrey-mamon@users.noreply.github.com>
2019-05-07 11:32:26 +0300

    Доработка файла

--- lesson-3.6.php
+++ lesson-3.6.php
@@ -1,36 +1,42 @@
 <?php
-
+ 
 $html = file_get_contents('index.html');
-
+ 
 $menu = [
     'Главная',
     'Новости',
-    'subMenu' => ['Новости о спорте', 'Новости о политеке', 'Новости о мире'],
+    ['Новости о спорте', 'Новости о политеке', 'Новости о мире'],
     'Контакты',
     'Справка',
 ];
-    
-echo "<ul>";
-foreach ($menu as $key => $item){
-    if ($key !== 'subMenu') {
-        echo "<li><a>{$item}</a></li>";
-        continue;
-    }
-    echo "<ul>";
-    if (is_array($item)) {
-        foreach ($item as $options) {
-            echo "<li><a>{$options}</a></li>";
-        }     
-    }
-    echo "</ul>";
+   
+function renderMenuItem($item) {
+    return "<li><a>{$item}</a></li>";
 }
-echo "</ul>";
-
-$html = preg_replace(
-    '/<nav>*<\\/nav>/',
-    '<h3>Меню</h3>',
-    $html
-);
+ 
+function renderMenuList($items) {
+    $menuString = "";
+    foreach ($items as $item) {
+        if (is_array($item)) {
+            $menuString .= renderMenuList($item);
+            continue;
+        }
+        $menuString .= renderMenuItem($item);
+    }    
+    return "<ul>{$menuString}</ul>";
+}
+ 
+function str_to_utf8 ($str) { 
+    if (mb_detect_encoding($str, 'UTF-8', true) === false) {
+        $str = mb_convert_encoding($str, "UTF-8");
+    }
+    return $str;
+}
+ 
+$menuHtml = renderMenuList($menu);
+ 
+$html = str_to_utf8($html);
+$html = str_replace('{Menu}', $menuHtml, $html);
 echo $html;
-
+ 
 ?>
